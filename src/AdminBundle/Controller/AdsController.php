738e80db2976ba5799abc4cf01b9fd8bf7d754ea
Roozbeh Baabakaan <baabakaan@remake.ir>
2016-05-21 16:34:33 +0430

    admin ads update
    admin ads list update

--- src/AdminBundle/Controller/AdsController.php
+++ src/AdminBundle/Controller/AdsController.php
@@ -9,7 +9,7 @@ class AdsController extends Controller {
 
     public function listAction(Request $request) {
         $adsService = $this->get($this->getParameter('yard_ads.ads_service'));
-        $list = $adsService->listAdsByPagintor($request->query->getInt('page', 1), 2) ;
+        $list = $adsService->listAdsByPagintor($request->query->getInt('page', 1), 10) ;
         return $this->render('AdminBundle:Ads:list.html.twig',['list'=>$list]);
     }
 
